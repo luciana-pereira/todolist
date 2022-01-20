33dec8f4f81d5fed8b2535b0d1a7dc44eaadb575
luciana-pereira <luci.lv14@gmail.com>
2022-01-20 00:10:57 -0300

    initial structure

---
+++ app/src/main/java/com/dio/to_do_list/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dio.to_do_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
